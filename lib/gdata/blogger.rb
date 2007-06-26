4f2fbc7028b8273aaac0ec84c3524aa60145a243
crkruse <crkruse@3d76ca7b-2e8e-4910-8bb0-bc18f4d68cdf>
2007-06-26 08:38:25 +0000

    Added multiple Blog support, and a method to retrieve user_id.  Also replaced hpricot requirement.
    git-svn-id: http://gdata-ruby.rubyforge.org/svn@11 3d76ca7b-2e8e-4910-8bb0-bc18f4d68cdf

--- lib/gdata/blogger.rb
+++ lib/gdata/blogger.rb
@@ -1,16 +1,43 @@
 require 'gdata/base'
 require 'builder'
+require 'hpricot'
+require 'rexml/document'
 
 module GData
 
-  class Blogger < GData::Base
-
-    def initialize(blog_id, entry_id=nil)
+  class Blogger < GData::Client
+    attr_reader :blog_id, :entry_id, :user_id, :blogs
+    attr_writer :blog_id, :entry_id
+    # Default initialization method.  The blog ID and the entry ID may 
+    # or may not be known ahead of time.
+    def initialize(blog_id=nil, entry_id=nil)
       @blog_id = blog_id
       @entry_id = entry_id
       super 'blogger', 'gdata-ruby', 'www.blogger.com'
     end
-
+    
+    # Pull down a list of the user's blogs.  This allows the use of muliple blogs
+    # per user.  The @blogs Array will store the available blogs by internal hash.
+    # 
+    def retrieve_blog_list
+      # retrieve the user's list of blogs from 
+      blog_feed = get('/feeds/default/blogs')
+      @blog_list = REXML::Document.new(blog_feed[1]).root
+      @blogs = Array.new
+      @blog_list.elements.each('entry'){|entry| @blogs.push({entry.elements['title'].get_text.to_s => entry.elements['id'].get_text.to_s.split(/blog-/).last})}
+      # By default, set the first blog to @blog_id
+      @blog_id = @blogs[0].values.to_s
+      @blogs
+    end
+    # retrieves the user$ ID from the blog_list feed.
+    def get_user_id
+      # Because someone might call this method without the blog_list called,
+      # we wouldn't have data to pull from.  So let's call that method.
+      self.blog_list
+      uid = @blog_list.elements['id'].get_text.to_s.split(/-|\:/)
+      @user_id = uid[uid.index("user")+1].delete(".blogs") 
+    end
+    
     def feed
       request "/feeds/#{@blog_id}/posts/default"
     end
