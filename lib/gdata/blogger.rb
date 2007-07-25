26728c66dbea138ed1cbfa1b1ce62e954a9e6c41
crkruse <crkruse@3d76ca7b-2e8e-4910-8bb0-bc18f4d68cdf>
2007-07-25 15:32:05 +0000

    Added selection of entry_id and added some comments.
    git-svn-id: http://gdata-ruby.rubyforge.org/svn@15 3d76ca7b-2e8e-4910-8bb0-bc18f4d68cdf

--- lib/gdata/blogger.rb
+++ lib/gdata/blogger.rb
@@ -6,40 +6,58 @@ require 'rexml/document'
 module GData
 
   class Blogger < GData::Client
-    attr_reader :blog_id, :entry_id, :user_id, :blogs
-    attr_writer :blog_id, :entry_id
+    attr_reader :blog_id, :entry_id, :blogs, :posts
     # Default initialization method.  The blog ID and the entry ID may 
     # or may not be known ahead of time.
-    def initialize(blog_id=nil, entry_id=nil)
-      @blog_id = blog_id
-      @entry_id = entry_id
+    def initialize(username, password, blog_id=nil, entry_id=nil)
       super 'blogger', 'gdata-ruby', 'www.blogger.com'
+      authenticate(username, password)
+      @entry_id = entry_id unless entry_id==nil
+      retrieve_blog_list unless blog_id != nil
     end
     
-    # Pull down a list of the user's blogs.  This allows the use of muliple blogs
-    # per user.  The @blogs Array will store the available blogs by internal hash.
-    # 
+    # Pull down a list of the user's blogs.  This allows the use of muliple
+    # blogs per user.  The @blogs Array will store the available blogs by
+    # internal hash.
+    # ex. @blogs[0] = {"Blog Name" => "blog_id(a string of numbers)"}
+    # returns @blogs - an outer program can use this to set the blog id using
+    # the set_blog_id method.
     def retrieve_blog_list
       # retrieve the user's list of blogs from 
       blog_feed = get('/feeds/default/blogs')
-      @blog_list = REXML::Document.new(blog_feed[1]).root
       @blogs = Array.new
-      @blog_list.elements.each('entry'){|entry| @blogs.push({entry.elements['title'].get_text.to_s => entry.elements['id'].get_text.to_s.split(/blog-/).last})}
-      # By default, set the first blog to @blog_id
-      @blog_id = @blogs[0].values.to_s
+      REXML::Document.new(blog_feed[1]).root.elements.each('entry') do |entry|
+        @blogs.push(entry.elements['id'].get_text.to_s.split(/blog-/).last)
+      end
+      set_blog_id(@blogs[0]) # set the initial blog ID to the first blog.
       @blogs
     end
-    # retrieves the user$ ID from the blog_list feed.
-    def get_user_id
-      # Because someone might call this method without the blog_list called,
-      # we wouldn't have data to pull from.  So let's call that method.
-      self.blog_list
-      uid = @blog_list.elements['id'].get_text.to_s.split(/-|\:/)
-      @user_id = uid[uid.index("user")+1].delete(".blogs") 
+
+    # Sets the current blog_id to the specified blog - use the @blogs variable
+    # to define the blog id!
+    # ex: b.set_blog_id(b.blogs[1])
+    def set_blog_id(blog)
+      @blog_id = blog
+      # Now we've changed blogs, but we also need to clear the entry id's and
+      # Post list.
+      @posts = nil
+      @entry_id = nil
+    end
+
+    # Retrieves the post feed from the blog contained in @blog_id.  Run through
+    # REXML, it returns an array of the different id's of that blog's posts.
+    def get_post_feed
+      post_feed = get "/feeds/#{@blog_id}/posts/default"
+      @posts = Array.new
+      REXML::Document.new(post_feed[1]).elements.each('feed/entry') do |entry|
+        @posts.push(entry.elements['id'].get_text.to_s.split(/post-/).last)
+      end
     end
     
-    def feed
-      request "/feeds/#{@blog_id}/posts/default"
+    # Sets the post (entry) id to the one specified.  Retrieve from the @posts
+    # variable.
+    def set_post_id(post)
+      @entry_id = post
     end
 
     def entry
